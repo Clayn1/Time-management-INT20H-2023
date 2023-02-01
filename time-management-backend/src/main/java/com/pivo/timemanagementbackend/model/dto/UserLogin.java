d90a28e1c621989626dd2f475dff13813fa9eb95
dako0221 <danylo.kotlov@netcracker.com>
2023-02-01 22:01:53 +0200

    Added Authorization

---
+++ time-management-backend/src/main/java/com/pivo/timemanagementbackend/model/dto/UserLogin.java
@@ -0,0 +1,14 @@
+package com.pivo.timemanagementbackend.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLogin {
+    private String email;
+    private String password;
+    private String name;
+}
---
+++ time-management-backend/src/main/java/com/pivo/timemanagementbackend/util/JwtTokenUtil.java
@@ -0,0 +1,55 @@
+package com.pivo.timemanagementbackend.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.function.Function;
+
+@Component
+public class JwtTokenUtil implements Serializable {
+    public static final int JWT_VALID_PERIOD = 2 * 60 * 60;
+
+    Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public String getUsernameFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        return claimsResolver.apply(getAllClaimsFromToken(token));
+    }
+
+    public Claims getAllClaimsFromToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setClaims(new HashMap<>())
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_VALID_PERIOD * 1000))
+                .signWith(key)
+                .compact();
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        return getUsernameFromToken(token).equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return getExpirationDateFromToken(token).before(new Date());
+    }
+}
